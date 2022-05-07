db45e69b805a92df7bd7f60bff812a1a76e29f69
skylineone044 <skylineone044@gmail.com>
2022-05-07 18:31:22 +0200

    it dont wanna work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 import { AngularFireModule } from '@angular/fire/compat';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { QuestionnaireComponent } from './pages/questionnaire/questionnaire.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+import { FormsModule } from '@angular/forms';
+
+import { LoginModule } from './pages/login/login.module';
+import { NotFoundModule } from './pages/not-found/not-found.module';
+import { SignupModule } from './pages/signup/signup.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionnaireComponent,
+    SidenavComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule,
+    AngularFireModule.initializeApp(environment.firebase),
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    FormsModule,
+    LoginModule,
+    NotFoundModule,
+    SignupModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
